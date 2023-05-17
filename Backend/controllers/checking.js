59e098828cd93e5ae393739cf9cca388f84576b2
vab096 <vabmanral@gmail.com>
2023-05-18 00:51:36 +0530

    get

--- Backend/controllers/checking.js
+++ Backend/controllers/checking.js
@@ -1,14 +1,30 @@
 const userSchema=require("../models/model");
 const express=require("express");
 const router=express.Router();
-
+const User=require("../models/model.js")
 router.post("",async(req,res)=>{
     // console.log(req.body)
     try{
-        const info=await userSchema.create(req.body); //if u don't use await postman return => {}(this curly brackets).
+        const info=await userSchema.create(req.body); 
+        //if u don't use await postman return => {}(this curly brackets).
         return res.status(201).send(info);
     }
     catch(err){
+        return res.status(500).send(err.message);
+    }
+})
+
+
+router.get("",async(req,res)=>{
+    // console.log(req.body)
+    try{
+        const data= await User.find().lean().exec()
+        //if u don't use await postman return => {}(this curly brackets).
+        // return res.status(201).send(info);
+        return res.status(201).send(info);
+    }
+    catch(err){
+        console.log(err.message);
         return res.status(500).send(err.message);
     }
 })
