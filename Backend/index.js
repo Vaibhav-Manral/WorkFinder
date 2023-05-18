cc05b510d5759f2d6bfe8cd2f218c43db43946cf
vab096 <vabmanral@gmail.com>
2023-05-18 17:47:12 +0530

    infos

--- Backend/index.js
+++ Backend/index.js
@@ -2,12 +2,11 @@ const express=require("express");
 const app=express();
 const checking=require("./controllers/checking");
 
-app.get("/infos",async(res,req)=>{
-    res.status(200).send("checked")
-})
+
 
 app.use(express.json());
 
 app.use("/users",checking)
+app.use("/infos",checking)
 
 module.exports=app;
